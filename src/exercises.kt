3cdb48eee3450e1d749628d8763f20c09d59fad6
cbot-course <cristiancbot@gmail.com>
2025-08-13 23:18:28 +0200

    Additional assignments.

---
+++ src/exercises.kt
@@ -0,0 +1,49 @@
+
+//constants
+
+val name: String = "cbot"
+
+//variables
+
+var age: Int = 11
+
+
+//Data Types
+
+
+//Numbers
+
+//Integer
+
+val number : Int = 31
+
+//Long
+
+val user_id : Long = 123456789L
+
+//Float
+
+val pi_2: Float = 3.14F
+
+//Double
+
+val temp: Double = 21.22
+
+//BOOLEAN
+// Two possible values, true or false
+
+val isActive: Boolean = false
+
+//TEXT
+//Char
+
+val letter: Char = 'A'
+
+//String \n -> new line, \t -> new tab
+
+val text: String = "What is this?"
+
+//ANY - data type for variable that should contain any type
+
+var what: Any = "hi"
+
