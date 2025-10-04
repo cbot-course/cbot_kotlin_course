1af2e1b62e35457eb95409162e33d3deb80b8003
cbot-course <cristiancbot@gmail.com>
2025-10-04 10:53:49 +0200

    Changing a lot of configs in order to get tests running without having to run them with maven only

---
+++ src/test/kotlin/com/cbot/course/chapter1/practice_unit_1_3/ExercisesUnit13Test.kt
@@ -0,0 +1,11 @@
+package com.cbot.course.chapter1.practice_unit_1_3
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ExercisesUnit13Test {
+    @Test
+    fun daysInWeekTest() {
+       assertEquals(7, daysInWeek)
+    }
+}
